e6711064faf45db2936c903240e338a523076494
Lucas Noguti <lucas.noguti3@gmail.com>
2025-08-30 21:43:53 -0300

    primeiro commit

---
+++ src/main/java/br/com/flatoys/FlatoysApplication.java
@@ -0,0 +1,13 @@
+package br.com.flatoys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatoysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatoysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/flatoys/FlatoysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flatoys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatoysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
